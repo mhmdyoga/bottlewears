bb5bc74a1e9c97db5766dc4d88ea50e2fab0f9d2
mhmdyoga <yogaskie@gmail.com>
2024-05-19 12:53:39 +0700

    updated project

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react'
 
-const Products = async () => {
+const Products = async() => {
    const response = await axios.get('http://localhost:8080/crockicle')
   const data = response.data;
   return (
