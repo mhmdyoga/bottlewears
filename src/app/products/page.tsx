b68320cca67c21b8236d005c6ce56da40f7e2bfb
mhmdyoga <yogaskie@gmail.com>
2024-05-19 14:28:48 +0700

    updated

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,28 +1,45 @@
-import axios from 'axios'
+"use client"
+import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react'
 
-const Products = async() => {
-   const response = await fetch('http://localhost:8080/crockicle')
-  const data = await response.json();
-  return (
-    <div>
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4  gap-5">
-          { data.map((items: any) => (
-            <Link href={`/crockicle/${items.id}`} className="ml-12 bg-sky-600 rounded-3xl" key={items.id}>
-              <Image src={items.img} alt="img" width={200} height={200}/>
-              <div className="text-white font-bold p-4">
-              <h2 className="text-start">{items.name}</h2>
-              <span className="text-start">Price: {items.price}</span>
-              </div>
-            </Link>
-           ))}
+const Products = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('http://localhost:8080/crockicle');
+                if (!response.ok) {
+                    throw new Error(`Network response was not ok: ${response.statusText}`);
+                }
+                const responseData = await response.json();
+                setData(responseData);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    return (
+        <div>
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+                {data.map((item) => (
+                    <Link href={`/crockicle/${item.id}`} className="ml-12 bg-sky-600 rounded-3xl" key={item.id}>
+                        <div>
+                            <Image src={item.img} alt="img" width={200} height={200}/>
+                            <div className="text-white font-bold p-4">
+                                <h2 className="text-start">{item.name}</h2>
+                                <span className="text-start">Price: {item.price}</span>
+                            </div>
+                        </div>
+                    </Link>
+                ))}
+            </div>
         </div>
-    </div>
-  )
-}
+    );
+};
 
-
-
-export default Products
+export default Products;
